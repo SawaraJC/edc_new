43c0f0635b802f3d2ea12e3b545510f847585521
Sanwara <sanwara.22110035@viit.ac.in>
2023-06-04 18:50:30 +0530

    Done with navbar and footer

--- new_web/src/App.jsx
+++ new_web/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <NavBar/>
+      {/* <NavBar/> */}
+      <Footer/>
     </>
   )
 }
