7d62087c4f18fafdf34e15bc3946719c3bec59d5
Sanwara <sanwara.22110035@viit.ac.in>
2023-07-16 17:15:07 +0530

    Changed email for nodemailer

--- new_web/backend/emailController.js
+++ new_web/backend/emailController.js
@@ -6,14 +6,14 @@ export default async function  emailController (data) {
     const transporter = createTransport({
         service: 'Gmail',
         auth:{
-            user:'sanwara.22110035@viit.ac.in',
-            pass:'Sawara26032002'
+            user:'nodemailertestedc@gmail.com',
+            pass:'nodemailer_edc'
         },
     });
 
     const mailOptions = {
         from: email,
-        to: 'sanwara.22110035@viit.ac.in',
+        to: 'nodemailertestedc@gmail.com',
         subject: 'Someone tried to contact EDC, VIIT',
         html: `<p>Name: ${name}</p><p>Email:${email}<p>Message: ${message}</p>`
     };
